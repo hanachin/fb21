e7a7344374a0c394df21677c899785a7af46d7f7
hanachin <hanachin@gmail.com>
2011-10-20 23:14:27 +0900

    first commit

---
+++ public_html/index.php
@@ -0,0 +1,13 @@
+<?php
+require '../src/facebook.php';
+require '../facebook_config.php';
+
+function h($s) {
+  return httpspecialchars($s, ENT_QUOTES);
+}
+
+$facebook = new Facebook($facebook_config);
+$session = $facebook->getSession();
+$fb_uid = $facebook->getUser();
+?>
+Welcome, your fb_uid is <span style="color:red;"><?= h($fb_uid) ?></span>
