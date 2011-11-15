b78a7f727b6b1976dd5793ac69cbd6f0584641d7
hanachin <hanachin@gmail.com>
2011-11-15 14:53:29 +0900

    move javascript code to like_ring.js from like_ring.html

--- public_html/like_ring.js
+++ public_html/like_ring.js
@@ -1,3 +1,22 @@
+window.fbAsyncInit = function() {
+    FB.init({
+        appId      : '206688072737420', // App ID
+        channelURL : '//fb21.fluxflex.com/channel.html', // Channel File
+        status     : true, // check login status
+        cookie     : true, // enable cookies to allow the server to access the session
+        oauth      : true, // enable OAuth 2.0
+        xfbml      : true  // parse XFBML
+    });
+};
+
+// Load the SDK Asynchronously
+(function(d){
+    var js, id = 'facebook-jssdk'; if (d.getElementById(id)) {return;}
+    js = d.createElement('script'); js.id = id; js.async = true;
+    js.src = "//connect.facebook.net/en_US/all.js";
+    d.getElementsByTagName('head')[0].appendChild(js);
+}(document));
+
 FB.api('/me', function(response) {
-  alert('Your name is ' + response.name);
+    alert('Your name is ' + response.name);
 });
